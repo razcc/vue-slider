5c007dff6971c31f1f4acebdb75d6fe28c6ca2fc
Rafael Leonardi <rafael.leonardi98@gmail.com>
2022-11-13 10:19:27 +0100

    Arrow Up and Down working

--- main.js
+++ main.js
@@ -11,21 +11,50 @@ var app = new Vue({
             "img/img2.jpg",
             "img/img3.jpg",
             "img/img4.jpg",
-        ]
+        ],
+        indice: 0,
+
 
     },
     methods: {
-        Img1Selection: function(){
-            this.imgPrincipale = "img/img1.jpg";
+        Img1Selection: function () {
+            this.imgPrincipale = this.imgArray[indice];
         },
-        Img2Selection: function(){
+        Img2Selection: function () {
             this.imgPrincipale = "img/img2.jpg";
         },
-        Img3Selection: function(){
+        Img3Selection: function () {
             this.imgPrincipale = "img/img3.jpg";
         },
-        Img4Selection: function(){
+        Img4Selection: function () {
             this.imgPrincipale = "img/img4.jpg";
         },
+
+        arrowDown: function () {
+            let lunghezza = this.imgArray.length - 1;
+            console.log(lunghezza)
+
+            if (this.indice >= lunghezza) {
+                return this.indice = 0;
+
+            } else {
+                return this.indice++;
+
+            }
+
+        },
+        arrowUP: function () {
+            let lunghezza = this.imgArray.length - 1;
+            console.log(lunghezza)
+            console.log(this.indice)
+            if (this.indice > lunghezza) {
+                return this.indice = 0;
+            } else if (this.indice == 0) {
+                return this.indice = 3;
+            } else {
+                return this.indice--;
+            }
+        }
     }
+
 })
